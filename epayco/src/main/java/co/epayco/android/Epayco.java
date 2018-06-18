6d23228af88003ae603fabaca8c959cff3b9bc72
pruebasepayco <40249987+pruebasepayco@users.noreply.github.com>
2018-06-18 17:09:05 -0500

    Update Epayco.java

--- epayco/src/main/java/co/epayco/android/Epayco.java
+++ epayco/src/main/java/co/epayco/android/Epayco.java
@@ -34,7 +34,7 @@ public class Epayco {
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
-    private static final String BASE_URL = "https://75bd3446.ngrok.io/";
+    private static final String BASE_URL = "https://75bd3446.ngrok.io";
     private static final String BASE_URL_SECURE = "https://secure.epayco.co";
 
     private String apiKey;
